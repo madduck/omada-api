081c42aeed67c00d80083e06521f98c2212b661c
martin f. krafft <madduck@madduck.net>
2024-06-27 22:37:45 +0200

    Hotspot voucher api interface
    Signed-off-by: martin f. krafft <madduck@madduck.net>

---
+++ vouchers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from omada import Omada
+
+def main():
+
+    omada = Omada(verbose=True)
+    omada.login()
+
+    try:
+        for vg in omada.getVoucherGroups():
+            if len(sys.argv) > 1 and not any(arg in vg['name'] for arg in sys.argv[1:]):
+                # if arguments are provided, limit the voucher groups searched
+                # to those whose names match one of the command line
+                # arguments. I.e. if there are arguments and none ("not any")
+                # match, then skip this group:
+                continue
+            unused = omada.getUnusedVouchers(vg['id'], maxnr=10)
+            codes = [v['code'] for v in unused]
+            print(f"{vg['name']}: {', '.join(codes)}")
+
+    finally:
+        omada.logout()
+
+if __name__ == '__main__':
+    import warnings
+    with warnings.catch_warnings(action="ignore"):
+        main()
